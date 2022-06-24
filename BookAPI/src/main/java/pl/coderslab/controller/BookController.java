0e1f68d7a1cfcb25e3e74c241108b0200f44d86f
Kamil <Kamil.Urbanik@o2.pl>
2022-06-24 10:57:53 +0200

    BookAPI - Release v0.1.0

---
+++ BookAPI/src/main/java/pl/coderslab/controller/BookController.java
@@ -0,0 +1,35 @@
+package pl.coderslab.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import pl.coderslab.model.Book;
+import pl.coderslab.service.BookService;
+import pl.coderslab.service.MockBookService;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping(value = "/books")
+public class BookController {
+    private BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @RequestMapping(value = "/helloBook")
+    public Book helloBook() {
+        return new Book(1L, "9788324631766", "Thinking in Java", "Bruce Eckel", "Helion", "programming");
+    }
+    @RequestMapping(value = "")
+    @ResponseBody
+    public List<Book> getList() {
+        return bookService.getBooks();
+    }
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Book> getBooksById(@PathVariable Long id) {
+        return bookService.getBookById(id);
+    }
+}
---
+++ BookAPI/src/main/java/pl/coderslab/service/MockBookService.java
@@ -0,0 +1,37 @@
+package pl.coderslab.service;
+
+import org.springframework.stereotype.Component;
+import pl.coderslab.model.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+@Component
+public class MockBookService implements BookService {
+    private List<Book> books;
+    public MockBookService() {
+        books = new ArrayList<>();
+        books.add(new Book(1L, "9788324631766", "Thiniking	in	Java", "Bruce	Eckel", "Helion", "programming"));
+        books.add(new Book(2L, "9788324627738", "Rusz	glowa	Java.", "Sierra	Kathy,	Bates	Bert", "Helion",
+                "programming"));
+        books.add(new Book(3L, "9780130819338", "Java	2.	Podstawy", "Cay	Horstmann,	Gary	Cornell", "Helion",
+                "programming"));
+    }
+
+
+    @Override
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    @Override
+    public List<Book> getBookById(Long id) {
+        List<Book> bookId = new ArrayList<>();
+        return  bookId = books.stream()
+                .filter(it -> id.equals(it.getId()))
+                .toList();
+        }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+}
